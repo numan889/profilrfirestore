8a223d9493b6b17384a59c1dbfbab0988bb99933
Numan <45208819+numan889@users.noreply.github.com>
2020-04-14 16:00:17 +0600

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,190 @@
+package com.dreamgarden.profilrfirestore;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Login extends AppCompatActivity {
+
+
+    private EditText userMail,userPassword;
+    private TextView forgotTextLink;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent MainActivity;
+    private ImageView loginPhoto;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.loginBtn);
+        forgotTextLink = findViewById(R.id.forgotPassword);
+        loginProgress = findViewById(R.id.login_progress);
+        mAuth = FirebaseAuth.getInstance();
+
+        MainActivity = new Intent(this,MainActivity.class);
+        loginPhoto = findViewById(R.id.login_photo);
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent registerActivity = new Intent(getApplicationContext(),Register.class);
+                startActivity(registerActivity);
+                finish();
+
+
+            }
+        });
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please Verify All Field");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    signIn(mail,password);
+                }
+
+
+
+
+            }
+        });
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                AlertDialog passwordResetDilog;
+                passwordResetDialog.setMessage("ENTER Your Email ID TO RECEIVED RESET Password LINK");
+                passwordResetDialog.setView(resetMail);
+
+
+                passwordResetDialog.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this, "Reset password Link send to your email", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error ! password Link Not send + e.getMassage()", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                passwordResetDialog.create(). show ();
+            }
+        });
+
+
+
+    }
+
+    private void signIn(String mail, String password) {
+
+
+        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                if (task.isSuccessful()) {
+
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+
+
+            }
+        });
+
+
+
+    }
+
+    private void updateUI() {
+
+        startActivity(MainActivity);
+        finish();
+
+    }
+
+    private void showMessage(String text) {
+
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null) {
+            //user is already connected  so we need to redirect him to home page
+            updateUI();
+
+        }
+
+
+
+    }
+}
